59752836c2774019634cf22e9120c5c2b7188ddc
Baptiste <estitpab@gmail.com>
2018-11-19 22:43:10 +0100

    Ajout des fichiers chapitre JSX

---
+++ memory/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({cards, feedback}) => (
+ <div className="card" />
+  <span className="symbol"></span>
+ </div>
+ )
+
+
+
+export default Card
